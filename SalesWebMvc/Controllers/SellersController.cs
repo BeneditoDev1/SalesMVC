809709d905b369c14c0f60b699b6552dab451390
Benedito Magalhães <rustic4225@gmail.com>
2022-07-22 23:49:24 -0300

    Seller details and eager loading

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -60,5 +60,21 @@ namespace SalesWebMvc.Controllers
             _sellersService.Remove(id);
             return RedirectToAction(nameof(Index));
         }
+        
+        public IActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var obj = _sellersService.FindById(id.Value);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+
+            return View(obj);
+        }
     }
 }
